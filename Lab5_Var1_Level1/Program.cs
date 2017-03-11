12073b9f87e476b4fcdc260b15b638c40ba19447
rchurianov <rchurianov@gmail.com>
2017-03-11 14:27:47 +0300

    initial solution

---
+++ Lab5_Var1_Level1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5_Var1_Level1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
